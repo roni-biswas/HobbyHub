c3942ed3a29c7c0e26896d85bdd069055e861d75
Roni Biswas <ronibiswas068@gmail.com>
2025-05-22 19:42:17 +0600

    create group and insert data in db

--- src/pages/CreateGroup.jsx
+++ src/pages/CreateGroup.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { use, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router";
 
 const categories = [
   "Drawing & Painting",
@@ -11,20 +14,65 @@ const categories = [
   "Writing",
 ];
 
-const CreateGroup = ({ user }) => {
+const CreateGroup = () => {
+  const { user } = use(AuthContext);
+  const [userData, setUserData] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user?.email) {
+      fetch(`http://localhost:3000/users/${user.email}`)
+        .then((res) => res.json())
+        .then((data) => setUserData(data))
+        .catch((err) => console.error("Error fetching user data:", err));
+    }
+  }, [user?.email]);
+
+  const handleCreateGroup = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const groupData = Object.fromEntries(formData.entries());
+
+    // create groups in db
+    fetch("http://localhost:3000/groups", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(groupData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Group Successfully Created!",
+            icon: "success",
+            draggable: true,
+          });
+          form.reset();
+          navigate("/my-group");
+        }
+      });
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center px-4 py-24 pt-32">
       <div className="bg-white/80 backdrop-blur-lg shadow-xl p-8 md:p-10 rounded-2xl w-full max-w-3xl border border-gray-200">
         <h2 className="text-3xl font-bold text-center text-primary mb-6">
           Create a New Hobby Group
         </h2>
-        <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <form
+          onSubmit={handleCreateGroup}
+          className="grid grid-cols-1 md:grid-cols-2 gap-6"
+        >
           <div className="form-control">
             <label className="label font-semibold text-base-300">
               Group Name
             </label>
             <input
               type="text"
+              name="group_name"
               placeholder="Enter group name"
               className="input input-bordered w-full"
             />
@@ -34,12 +82,14 @@ const CreateGroup = ({ user }) => {
             <label className="label font-semibold text-base-300">
               Hobby Category
             </label>
-            <select className="select select-bordered w-full">
+            <select name="category" className="select select-bordered w-full">
               <option disabled selected>
                 Select a category
               </option>
               {categories.map((category, idx) => (
-                <option key={idx}>{category}</option>
+                <option key={idx} defaultValue={category}>
+                  {category}
+                </option>
               ))}
             </select>
           </div>
@@ -51,6 +101,7 @@ const CreateGroup = ({ user }) => {
               </label>
               <input
                 type="text"
+                name="description"
                 className="input input-bordered w-full py-0"
                 placeholder="Write a short description..."
               ></input>
@@ -63,6 +114,7 @@ const CreateGroup = ({ user }) => {
             </label>
             <input
               type="text"
+              name="location"
               placeholder="Enter location"
               className="input input-bordered w-full"
             />
@@ -74,6 +126,7 @@ const CreateGroup = ({ user }) => {
             </label>
             <input
               type="number"
+              name="max_members"
               placeholder="e.g. 10"
               className="input input-bordered w-full"
             />
@@ -83,7 +136,11 @@ const CreateGroup = ({ user }) => {
             <label className="label font-semibold text-base-300">
               Start Date
             </label>
-            <input type="date" className="input input-bordered w-full" />
+            <input
+              type="date"
+              name="date"
+              className="input input-bordered w-full"
+            />
           </div>
 
           <div className="form-control">
@@ -92,6 +149,7 @@ const CreateGroup = ({ user }) => {
             </label>
             <input
               type="url"
+              name="photo_url"
               placeholder="https://example.com/image.jpg"
               className="input input-bordered w-full"
             />
@@ -103,7 +161,8 @@ const CreateGroup = ({ user }) => {
             </label>
             <input
               type="text"
-              value={user?.name || "Guest"}
+              name="name"
+              value={userData?.name || "Guest"}
               readOnly
               className="input input-bordered bg-gray-100 text-gray-500 w-full"
             />
@@ -115,14 +174,15 @@ const CreateGroup = ({ user }) => {
             </label>
             <input
               type="email"
-              value={user?.email || "guest@example.com"}
+              name="email"
+              value={userData?.email || "guest@example.com"}
               readOnly
               className="input input-bordered bg-gray-100 text-gray-500 w-full"
             />
           </div>
 
           <div className="form-control md:col-span-2 mt-4">
-            <button className="btn btn-secondary w-full text-lg">
+            <button type="submit" className="btn btn-secondary w-full text-lg">
               Create Group
             </button>
           </div>
